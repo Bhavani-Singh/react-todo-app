3bcca135db1ba16f5910e681fb2d2f28d00b90a1
BhavaniSingh <bhavanisinghrajput24@gmail.com>
2023-09-28 10:56:54 +0530

    add insert and delete functionality and store the updated results in local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import {AiOutlineDelete} from 'react-icons/ai';
 import {AiOutlineCheck} from 'react-icons/ai';
 
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTodos, setTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    }
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newTodoItem);
+    setTodos(updatedTodoArr);
+    localStorage.setItem("todoList", JSON.stringify(updatedTodoArr));
+  };
+
+  const handleDeleteTodo = (index) => {
+    let reduceTodo = [...allTodos];
+    reduceTodo.splice(index, 1);
+
+    localStorage.setItem('todoList',JSON.stringify(reduceTodo));
+    setTodos(reduceTodo);
+  };
+
+  useEffect(() => {
+    let savedTodo = JSON.parse(localStorage.getItem("todoList"));
+    if(savedTodo) {
+      setTodos(savedTodo);
+    }
+  }, [])
+
   return (
     <div className="App">
       <h1>My Todos</h1>
@@ -13,16 +44,16 @@ function App() {
         <div className='todo-input'>
           <div className='todo-input-item'>
             <label>Title</label>
-            <input type='text' placeholder="What's the task title?" />
+            <input type='text' value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="What's the task title?" />
           </div>
 
           <div className='todo-input-item'>
             <label>Description</label>
-            <input type='text' placeholder="What's the task description?" />
+            <input type='text' value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder="What's the task description?" />
           </div>
 
           <div className='todo-input-item'>
-            <button type='button' className='primaryBtn'>Add</button>
+            <button type='button' onClick={handleAddTodo} className='primaryBtn'>Add</button>
           </div>
         </div>
 
@@ -32,22 +63,23 @@ function App() {
         </div>
 
         <div className='todo-list'>
-          <div className='todo-list-item'>
-            <div>
-              <h3>task 1</h3>
-              <p>Description</p>
-            </div>
-            
-            <div>
-            <AiOutlineDelete className='icon'/>
-            <AiOutlineCheck className='check-icon'/>
+          {allTodos.map((item, index) => {
+            return(
+              <div className='todo-list-item' key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div>
+                  <AiOutlineDelete className='icon' onClick={()=>handleDeleteTodo(index)}/>
+                  <AiOutlineCheck className='check-icon'/>
+                </div>
+              </div>
+            )
+          })}
           </div>
-          </div>
-
-          
         </div>
       </div>
-    </div>
   );
 }
 
